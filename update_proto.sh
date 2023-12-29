2f418c75a8b73e86c96d77a5f2b55b2d8cd3698d
Thomas Armena <thomasarmena@gmail.com>
2023-12-29 17:41:12 -0500

    update proto script

--- update_proto.sh
+++ update_proto.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+mkdir lib/protos/
+mkdir server/cozyworld/protos/
+
 protoc --proto_path=protos/ --go_out=server/cozyworld/protos/ --go_opt=paths=source_relative protos/*.proto
 protoc --proto_path=protos/ --dart_out=lib/protos/ protos/*.proto
 
